2811cf4c2f476ac2142909bb1275daef0aff07ac
Oliver <oliver-mx@gmx.de>
2026-07-23 11:56:56 +0200

    Make.jl updated

--- docs/make.jl
+++ docs/make.jl
@@ -1,11 +1,5 @@
 using TermiteMoundInducedAirflowTrixi
 using Documenter
-using DocumenterInterLinks
-using Literate
-using Changelog: Changelog
-
-# Provide external links (project root and inventory file)
-links = InterLinks("TermiteMoundInducedAirflowTrixi" => ("https://oliver-mx.github.io/TermiteMoundInducedAirflowTrixi.jl/"))
 
 DocMeta.setdocmeta!(
     TermiteMoundInducedAirflowTrixi,
@@ -14,14 +8,22 @@ DocMeta.setdocmeta!(
     recursive = true,
 )
 
-# Copy contents form README to the starting page to not need to synchronize it manually
-readme_text = read(joinpath(dirname(@__DIR__), "README.md"), String)
-readme_text = replace(readme_text,
-                      "[LICENSE.md](LICENSE.md)" => "[License](@ref)",
-                      "<p" => "```@raw html\n<p",
-                      "p>" => "p>\n```",
-                      r"\[comment\].*\n" => "")    # remove comments
-write(joinpath(@__DIR__, "src", "home.md"), readme_text)
+makedocs(;
+    modules = [TermiteMoundInducedAirflowTrixi],
+    authors = "Oliver Peter Marx <oliver-mx@uni-hamburg.de>",
+    sitename = "TermiteMoundInducedAirflowTrixi.jl",
+    format = Documenter.HTML(;
+        canonical = "https://oliver-mx.github.io/TermiteMoundInducedAirflowTrixi.jl",
+        edit_link = "master",
+        assets = String[],
+    ),
+    pages = ["Home" => "index.md"],
+)
+
+deploydocs(;
+    repo = "github.com/oliver-mx/TermiteMoundInducedAirflowTrixi.jl",
+    devbranch = "main",
+)
 
 makedocs(;
     modules = [TermiteMoundInducedAirflowTrixi],
@@ -36,10 +38,9 @@ makedocs(;
         "Installation" => "installation.md",
         "Introduction" => "introduction.md",
         "License" => "license.md",
-        "Reference" => "reference.md"],
-    plugins = [links],)
+        "Reference" => "reference.md"],)
 
 deploydocs(;
     repo = "github.com/oliver-mx/TermiteMoundInducedAirflowTrixi.jl",
-    devbranch = "main",
+    devbranch = "master",
 )
