c87961c4168060a2192d6ec7f1acab60a4679439
Oliver <oliver-mx@gmx.de>
2026-07-23 11:46:20 +0200

    Updated make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,11 @@
 using TermiteMoundInducedAirflowTrixi
 using Documenter
+using DocumenterInterLinks
+using Literate
+using Changelog: Changelog
+
+# Provide external links (project root and inventory file)
+links = InterLinks("TermiteMoundInducedAirflowTrixi" => ("https://oliver-mx.github.io/TermiteMoundInducedAirflowTrixi.jl/"))
 
 DocMeta.setdocmeta!(
     TermiteMoundInducedAirflowTrixi,
@@ -7,6 +13,15 @@ DocMeta.setdocmeta!(
     :(using TermiteMoundInducedAirflowTrixi);
     recursive = true,
 )
+
+# Copy contents form README to the starting page to not need to synchronize it manually
+readme_text = read(joinpath(dirname(@__DIR__), "README.md"), String)
+readme_text = replace(readme_text,
+                      "[LICENSE.md](LICENSE.md)" => "[License](@ref)",
+                      "<p" => "```@raw html\n<p",
+                      "p>" => "p>\n```",
+                      r"\[comment\].*\n" => "")    # remove comments
+write(joinpath(@__DIR__, "src", "home.md"), readme_text)
 
 makedocs(;
     modules = [TermiteMoundInducedAirflowTrixi],
@@ -17,10 +32,14 @@ makedocs(;
         edit_link = "master",
         assets = String[],
     ),
-    pages = ["Home" => "index.md"],
-)
+    pages = ["Home" => "index.md", 
+        "Installation" => "installation.md",
+        "Introduction" => "introduction.md",
+        "License" => "license.md",
+        "Reference" => "reference.md"],
+    plugins = [links],)
 
 deploydocs(;
     repo = "github.com/oliver-mx/TermiteMoundInducedAirflowTrixi.jl",
-    devbranch = "master",
+    devbranch = "main",
 )
